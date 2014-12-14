28a8018a1a8df9a94d12628f248dc8831075c95a
Longray <Longray@126.com>
2014-12-15 01:30:35 +0800

    把日历，日期选择器移植过来。

--- ModernUI/Properties/AssemblyInfo.cs
+++ ModernUI/Properties/AssemblyInfo.cs
@@ -1,12 +1,16 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Security;
 using System.Windows;
 
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
+using System.Windows.Markup;
+
 [assembly: AssemblyTitle("ModernUI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -53,3 +57,16 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+
+//// Needed to turn on checking of security critical call chains
+[assembly: SecurityCritical]
+
+//// Needed to enable xbap scenarios
+//[assembly: AllowPartiallyTrustedCallers]
+
+//[assembly: CLSCompliant(true)]
+//[assembly: NeutralResourcesLanguage("en-US")]
+
+//[assembly: XmlnsDefinition("http://schemas.abchina.com/wpf/2014/mui", "ModernUI.Windows.Controls")]
+//[assembly: XmlnsDefinition("http://schemas.abchina.com/wpf/2014/mui", "ModernUI.Windows.Controls.Primitives")]
